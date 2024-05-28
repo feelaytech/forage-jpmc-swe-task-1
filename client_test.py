9927234bc96f1422fe94f140b043af4c27bba53e
thinktank01 <felixayot@gmail.com>
2024-05-28 23:56:48 +0300

    Add tests in for client3.py methods Tests added: Tested getDataPoint price values and getRatio return values. Tested that getRatio does not throw a ZeroDivisionError

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    assert getDataPoint(quotes[0]) == ('ABC', 120.48, 121.2, 120.84)
+    assert getDataPoint(quotes[1]) == ('DEF', 117.87, 121.68, 119.775)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +17,17 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    assert getDataPoint(quotes[0]) == ('ABC', 120.48, 119.2, 119.84)
+    assert getDataPoint(quotes[1]) == ('DEF', 117.87, 121.68, 119.775)
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    assert getRatio(120.84, 119.775) == 120.84 / 119.775
+    assert getRatio(119.84, 119.75) == 119.84 / 119.75
 
-
+  def test_getRatio_calculateRatioRaisesNoDivisionError(self):
+    assert getRatio(121.75, 0) == None
 
 if __name__ == '__main__':
     unittest.main()
